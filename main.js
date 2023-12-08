8c82f45fe4b1caa0f4d791349df615488774bd6c
BRIGHT OMOLAYO <molayobright@gmail.com>
2023-12-08 13:36:22 +0000

    side dropdown

---
+++ main.js
@@ -0,0 +1,20 @@
+function toggleContent() {
+    var div = document.getElementsByClassName("dropbtn");
+    var content = document.getElementById("myDropdown");
+  
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  }
+  function toggleContent() {
+    var div = document.getElementsByClassName("mastering");
+    var content = document.getElementById("myDropdown2");
+  
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  }
