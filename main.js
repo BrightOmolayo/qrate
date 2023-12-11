062a2ca44683e742f02b612bb1c739233f724c96
BRIGHT OMOLAYO <molayobright@gmail.com>
2023-12-11 14:30:52 +0000

    footer

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 function myFunction(){
   var x = document.getElementById("dropdown1");
 
-  var y = document.getElementById("link1");
-      y.textContent= "LEARN MORE";
+  var link1 = document.getElementById("link1");
+      link1.textContent= "Records";
 
-  var p = document.getElementById("p")
-      p.textContent= "With over 40 colors and over 120,000 variations, control what your record will look like."
+  var link2 = document.getElementById("link2");
+      link2.textContent= "Cassettes";
+
+  var link3 = document.getElementById("link3");
+      link3.textContent= "Stories";
+
+  var link4 = document.getElementById("link4");
+      link4.textContent= "Qrates Curated";
 
   if(x.style.display === "none"){
     x.style.display = "block";
@@ -13,6 +19,21 @@ function myFunction(){
    else{
      x.style.display = "none";
   }
+}
+
+function toggleSection(element) {
+  var content = element.nextElementSibling;
+
+  // Close all other sections before opening the clicked one
+  var allSections = document.querySelectorAll('.accordion-content');
+  allSections.forEach(function (section) {
+    if (section !== content && section.classList.contains('show')) {
+      section.classList.remove('show');
+    }
+  });
+
+  // Toggle the 'show' class to display or hide the content
+  content.classList.toggle('show');
 }
 
 
